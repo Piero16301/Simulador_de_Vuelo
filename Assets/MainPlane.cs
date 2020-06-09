c2eeb5dfeb89b7f9876a94c732ded132fed1fb8a
Piero Morales <piero.morales@utec.edu.pe>
2020-06-08 22:57:27 -0500

    Se comienza el envío de datos serial

--- Assets/MainPlane.cs
+++ Assets/MainPlane.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using System.IO.Ports;
+using System;
 
 public class MainPlane : MonoBehaviour
 {
@@ -9,27 +12,41 @@ public class MainPlane : MonoBehaviour
 	public bool cameraSwitch = false;
 	public Canvas FirstPersonCanvas;
 	Camera ThirdPersonCam;
-	
+
+	SerialPort arduinoPort = new SerialPort("COM8");
+
+	private void Awake()
+	{
+		arduinoPort.BaudRate = 9600;
+		arduinoPort.Parity = Parity.None;
+		arduinoPort.StopBits = StopBits.One;
+		arduinoPort.DataBits = 8;
+		arduinoPort.Handshake = Handshake.None;
+	}
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		Debug.Log("plane pilot script added to: " + gameObject.name);
+		UnityEngine.Debug.Log("plane pilot script added to: " + gameObject.name);
 		ThirdPersonCam = Camera.main;
 		FirstPersonCanvas.gameObject.SetActive(false);
 		ThirdPersonCam.enabled = true;
 		FirstPersonCam.enabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.V)) {
-			if (FirstPersonCam.enabled) {
+		if (Input.GetKeyDown(KeyCode.V))
+		{
+			if (FirstPersonCam.enabled)
+			{
 				ThirdPersonCam.enabled = true;
 				FirstPersonCam.enabled = false;
 				FirstPersonCanvas.gameObject.SetActive(false);
 			}
-			else if (!FirstPersonCam.enabled) {
+			else if (!FirstPersonCam.enabled)
+			{
 				ThirdPersonCam.enabled = false;
 				FirstPersonCam.enabled = true;
 				FirstPersonCanvas.gameObject.SetActive(true);
@@ -41,30 +58,45 @@ public class MainPlane : MonoBehaviour
 		ThirdPersonCam.transform.position = ThirdPersonCam.transform.position * bias +
 			moveCamTo * (1.0f - bias);
 		ThirdPersonCam.transform.LookAt(transform.position + transform.forward * 50.0f);
-		
+
 		transform.position += transform.forward * Time.deltaTime * speed;
-		
+
 		speed -= transform.forward.y * Time.deltaTime * 50.0f;
 
-		if (Input.GetAxis ("Speed") != 0.0f) {
-			speed += Input.GetAxis ("Speed") * Time.deltaTime * 20.0f;
+		if (Input.GetAxis("Speed") != 0.0f)
+		{
+			speed += Input.GetAxis("Speed") * Time.deltaTime * 20.0f;
 		}
 
-		/*if (speed < 0.0f) {
+		if (speed < 0.0f) {
 			speed = 0.0f;
-		}*/
-		
-		if (speed > 100.0f) {
-			speed = 100.0f;
 		}
-		
+
+		if (speed > 200.0f)
+		{
+			speed = 200.0f;
+		}
+
 		transform.Rotate(0.15f * Input.GetAxis("Vertical"), 0.1f * Input.GetAxis("Direction"), 0.2f * -Input.GetAxis("Horizontal"));
+
+		UnityEngine.Debug.Log("X:" + Math.Round(transform.rotation.eulerAngles.x, 2) + " Y:" + Math.Round(transform.rotation.eulerAngles.y, 2) + " Z:" + Math.Round(transform.rotation.eulerAngles.z, 2));
 		
+		//SendMessageToArduino("X:" + Math.Round(transform.rotation.eulerAngles.x, 2) + " Y:" + Math.Round(transform.rotation.eulerAngles.y, 2) + " Z:" + Math.Round(transform.rotation.eulerAngles.z, 2));
+
 		float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
-		if (terrainHeightWhereWeAre > transform.position.y) {
-			transform.position = new Vector3(transform.position.x,
-			                                 terrainHeightWhereWeAre + 2,
-			                                 transform.position.z);
+		if (terrainHeightWhereWeAre > transform.position.y)
+		{
+			transform.position = new Vector3(transform.position.x, terrainHeightWhereWeAre + 2, transform.position.z);
 		}
+	}
+
+	public void SendMessageToArduino(string message)
+	{
+		arduinoPort.WriteLine(message);
+	}
+
+	public void ClosePort()
+	{
+		arduinoPort.Close();
 	}
 }
